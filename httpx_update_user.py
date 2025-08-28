18c4e700c0b6f785b30df3888e768348329a872d
ViktorMityukov <vmityukov@innoseti.ru>
2025-08-28 18:00:12 +0200

    Практикуемся в работе с HTTPX. Практикуемся в работе с HTTPX: обновление пользователя

---
+++ httpx_update_user.py
@@ -0,0 +1,45 @@
+from tools.fakers import get_random_email
+import httpx
+
+
+url = "http://127.0.0.1:8000"
+def create_user():
+    end_point = url + "/api/v1/users"
+    payload = {
+        "email": get_random_email(),
+        "password": "12345",
+        "lastName": "string",
+        "firstName": "string",
+        "middleName": "string"
+    }
+    resp = httpx.post(end_point, json=payload)
+    print(resp.status_code)
+    return resp.json()['user']['id'], payload
+
+def authentication_to_system(user_info):
+    end_point = url + "/api/v1/authentication/login"
+    payload = {
+        "email": user_info['email'],
+        "password": user_info['password']
+    }
+    resp = httpx.post(end_point, json=payload)
+    print(resp.status_code)
+    return resp.json()["token"]["accessToken"]
+
+def patch_user(user_id, token):
+    end_point = url + f"/api/v1/users/{user_id}"
+    headers = {"Authorization": f"Bearer {token}"}
+    payload = {
+        "email": get_random_email(),
+        "lastName": "string",
+        "firstName": "string",
+        "middleName": "string"
+    }
+    resp = httpx.patch(end_point, headers=headers, json=payload)
+    print(resp.status_code)
+
+
+if __name__ == "__main__":
+    user_id, create_payload = create_user()
+    token = authentication_to_system(create_payload)
+    patch_user(user_id, token)
