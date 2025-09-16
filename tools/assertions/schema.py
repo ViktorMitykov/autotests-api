2f03b6a40e61559ace2ebfcb649e9b0827db2b26
ViktorMityukov <vmityukov@innoseti.ru>
2025-09-16 16:18:01 +0200

    Валидация JSON schema

---
+++ tools/assertions/schema.py
@@ -0,0 +1,10 @@
+from jsonschema import validate
+from typing import Any
+from jsonschema.validators import Draft202012Validator
+
+def validate_json_schema(instance: Any, schema: dict) -> None:
+    validate(
+        instance=instance,
+        schema = schema,
+        format_checker=Draft202012Validator.FORMAT_CHECKER
+    )
